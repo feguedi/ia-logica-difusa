4524a500502e5bee80147d9104676789831717e8
Eduardo Fernández (DY) <feguedi@yahoo.com.mx>
2019-05-30 21:00:35 -0500

    Funciones de evaluación

--- simple/js/datos.js
+++ simple/js/datos.js
@@ -1,11 +1,28 @@
-const enfermedades = [
-    {
+const enfermedades = [{
         "id": "diabetes",
         "nombre": "Diabetes mellitus (inicial)",
         "descripcion": "Primeros 2 años sin diagnóstico",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 6,
+            "cant-alimento": 7,
+            "somnolencia": 2,
+            "sobrepeso": 6,
+            "cansancio": 4,
+            "ejercicio": 2,
+            "insomnio": 1,
+            "fa-concentracion": 4,
+            "dis-memoria": 3,
+            "dis-auditiva": 4,
+            "dis-visual": 4,
+            "dental": 4,
+            "fracturas": 0,
+            "lumbalgia": 3,
+            "friolento": 1,
+            "polineuritis": 3,
+            "cabello": 4,
+            "ap-canas": 4,
+            "ap-arrugas": 3,
         }
     },
     {
@@ -14,7 +31,25 @@ const enfermedades = [
         "descripcion": "Adelgaza demasiado, pero no come lo suficiente porque se siente gorda",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 4,
+            "cant-alimento": 2,
+            "somnolencia": 7,
+            "sobrepeso": 1,
+            "cansancio": 7,
+            "ejercicio": 0,
+            "insomnio": 7,
+            "fa-concentracion": 8,
+            "dis-memoria": 9,
+            "dis-auditiva": 8,
+            "dis-visual": 8,
+            "dental": 6,
+            "fracturas": 8,
+            "lumbalgia": 6,
+            "friolento": 8,
+            "polineuritis": 4,
+            "cabello": 9,
+            "ap-canas": 9,
+            "ap-arrugas": 8,
         }
     },
     {
@@ -23,7 +58,25 @@ const enfermedades = [
         "descripcion": "Come mucha comida, sobre todo productos calóricos, luego se provoca el vómito",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 8,
+            "cant-alimento": 8,
+            "somnolencia": 7,
+            "sobrepeso": 1,
+            "cansancio": 8,
+            "ejercicio": 0,
+            "insomnio": 7,
+            "fa-concentracion": 8,
+            "dis-memoria": 8,
+            "dis-auditiva": 8,
+            "dis-visual": 8,
+            "dental": 8,
+            "fracturas": 9,
+            "lumbalgia": 8,
+            "friolento": 8,
+            "polineuritis": 8,
+            "cabello": 8,
+            "ap-canas": 8,
+            "ap-arrugas": 8,
         }
     },
     {
@@ -32,7 +85,25 @@ const enfermedades = [
         "descripcion": "3 años posteriores al primer diagnóstico",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 6,
+            "cant-alimento": 6,
+            "somnolencia": 7,
+            "sobrepeso": 7,
+            "cansancio": 1,
+            "ejercicio": 3,
+            "insomnio": 9,
+            "fa-concentracion": 4,
+            "dis-memoria": 4,
+            "dis-auditiva": 4,
+            "dis-visual": 4,
+            "dental": 4,
+            "fracturas": 0,
+            "lumbalgia": 4,
+            "friolento": 4,
+            "polineuritis": 3,
+            "cabello": 4,
+            "ap-canas": 4,
+            "ap-arrugas": 4,
         }
     },
     {
@@ -41,7 +112,25 @@ const enfermedades = [
         "descripcion": "",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 6,
+            "cant-alimento": 6,
+            "somnolencia": 7,
+            "sobrepeso": 7,
+            "cansancio": 9,
+            "ejercicio": 0,
+            "insomnio": 5,
+            "fa-concentracion": 7,
+            "dis-memoria": 5,
+            "dis-auditiva": 5,
+            "dis-visual": 4,
+            "dental": 5,
+            "fracturas": 3,
+            "lumbalgia": 7,
+            "friolento": 7,
+            "polineuritis": 5,
+            "cabello": 9,
+            "ap-canas": 9,
+            "ap-arrugas": 5,
         }
     },
     {
@@ -50,7 +139,25 @@ const enfermedades = [
         "descripcion": "Dolores - inflamación articular",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 4,
+            "cant-alimento": 4,
+            "somnolencia": 5,
+            "sobrepeso": 6,
+            "cansancio": 8,
+            "ejercicio": 2,
+            "insomnio": 5,
+            "fa-concentracion": 1,
+            "dis-memoria": 1,
+            "dis-auditiva": 3,
+            "dis-visual": 3,
+            "dental": 7,
+            "fracturas": 3,
+            "lumbalgia": 7,
+            "friolento": 7,
+            "polineuritis": 4,
+            "cabello": 7,
+            "ap-canas": 7,
+            "ap-arrugas": 3,
         }
     },
     {
@@ -59,7 +166,25 @@ const enfermedades = [
         "descripcion": "Tres años posteriores al primer diagnóstico",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 5,
+            "cant-alimento": 6,
+            "somnolencia": 4,
+            "sobrepeso": 5,
+            "cansancio": 0,
+            "ejercicio": 2,
+            "insomnio": 0,
+            "fa-concentracion": 0,
+            "dis-memoria": 1,
+            "dis-auditiva": 3,
+            "dis-visual": 3,
+            "dental": 3,
+            "fracturas": 9,
+            "lumbalgia": 4,
+            "friolento": 5,
+            "polineuritis": 3,
+            "cabello": 5,
+            "ap-canas": 4,
+            "ap-arrugas": 1,
         }
     },
     {
@@ -68,7 +193,25 @@ const enfermedades = [
         "descripcion": "Rebasa 30% del peso recomendado",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 7,
+            "cant-alimento": 8,
+            "somnolencia": 8,
+            "sobrepeso": 8,
+            "cansancio": 7,
+            "ejercicio": 1,
+            "insomnio": 7,
+            "fa-concentracion": 5,
+            "dis-memoria": 3,
+            "dis-auditiva": 5,
+            "dis-visual": 4,
+            "dental": 3,
+            "fracturas": 3,
+            "lumbalgia": 5,
+            "friolento": 4,
+            "polineuritis": 3,
+            "cabello": 3,
+            "ap-canas": 3,
+            "ap-arrugas": 1,
         }
     },
     {
@@ -77,13 +220,30 @@ const enfermedades = [
         "descripcion": "Nivel de salud 30 años mayor a la edad biológica",
         "recomendacion": "",
         "sintomas": {
-            
+            "hambre": 7,
+            "cant-alimento": 7,
+            "somnolencia": 8,
+            "sobrepeso": 5,
+            "cansancio": 8,
+            "ejercicio": 0,
+            "insomnio": 8,
+            "fa-concentracion": 8,
+            "dis-memoria": 7,
+            "dis-auditiva": 7,
+            "dis-visual": 7,
+            "dental": 3,
+            "fracturas": 1,
+            "lumbalgia": 5,
+            "friolento": 5,
+            "polineuritis": 5,
+            "cabello": 3,
+            "ap-canas": 3,
+            "ap-arrugas": 7,
         }
     }
 ];
 
-const sintomas = [
-    {
+const sintomas = [{
         "id": "hambre",
         "nombre": "Hambre",
         "descripcion": "",
--- simple/js/main.js
+++ simple/js/main.js
@@ -4,7 +4,7 @@ let generalContent = $('#general-content');
 let especificoContent = $('#especifico-content');
 let resultadoContent = $('#resultados-content');
 
-const umbral = 0.7;
+const umbral = 7;
 let sumaMembresias = 0;
 
 $('#boton-menu-especifico').click(() => {
@@ -41,16 +41,20 @@ const main = () => {
         cambiarEstadoElemento(menu);
     }, 2000);
 
+    // TODO: Cargar primero el manual
+    // después el menú
+
     cargarSintomas();
     cargarEnfermedades();
 }
 
 const cargarEnfermedades = () => {
     // 
-    let enfermedadesContainer = document.getElementById('#enfermedades-container');
+    let enfermedadesContainer = document.getElementById('enfermedades-container');
     let h = ``;
 
     enfermedades.forEach(enfermedad => {
+        console.log(`cargarEnfermedades: Cargando item: ${ enfermedad.nombre }`);
         let item = `
         <label class="mt-3">
             <input type="checkbox" name="enfermedad" class="card-input-element d-none" id="${ enfermedad.id }-checkbox">
@@ -59,7 +63,9 @@ const cargarEnfermedades = () => {
             </div>
         </label>
         `;
+        h += item;
     });
+    enfermedadesContainer.innerHTML = h;
 }
 
 const cargarSintomas = () => {
@@ -80,7 +86,7 @@ const cargarSintomas = () => {
                 <small id="slider-general-${ sintoma.id }-int">Nunca</small>
               </div>
               <div class="slidercontainer">
-                <input id="slider-general-${ sintoma.id }" type="range" min="0" max="100" value="0" class="slider slider-general" onchange="actualizarSlider(this)" oninput="actualizarSlider(this)" />
+                <input id="slider-general-${ sintoma.id }" type="range" step="10" min="0" max="100" value="0" class="slider slider-general" onchange="actualizarSlider(this)" oninput="actualizarSlider(this)" />
               </div>
             </div>
             `;
@@ -91,7 +97,7 @@ const cargarSintomas = () => {
                 <small id="slider-especifico-${ sintoma.id }-int">Nunca</small>
               </div>
               <div class="slidercontainer">
-                <input id="slider-especifico-${ sintoma.id }" type="range" min="0" max="100" value="0" class="slider slider-especifico" onchange="actualizarSlider(this)" oninput="actualizarSlider(this)" />
+                <input id="slider-especifico-${ sintoma.id }" type="range" step="10" min="0" max="100" value="0" class="slider slider-especifico" onchange="actualizarSlider(this)" oninput="actualizarSlider(this)" />
               </div>
             </div>
             `;
@@ -107,13 +113,14 @@ const cambiarEstadoElemento = elm => { elm.toggleClass("elm-oculto"); }
 
 const evaluacionGeneral = () => {
     let slidersSintomas = $('.slider-general');
-    let evSliders = [], resultado = [];
+    let modalResultados = $('#modal-resultados')
+    let evSliders = [], resultados = [];
 
     [...slidersSintomas].forEach(val => {
         evSliders.push(
             {
-                "nombre": val.id,
-                "valor": Number(val.value / 100)
+                "id": val.id.slice(15),
+                "valor": Number(val.value / 10)
             }
         );
     });
@@ -121,14 +128,68 @@ const evaluacionGeneral = () => {
     enfermedades.forEach(enfermedad => {
         sumaMembresias = 0;
         evSliders.forEach(sintoma => {
-            sumaMembresias += Math.min(enfermedad.sintomas[sintoma], sintoma.valor);
+            sumaMembresias += Math.min(enfermedad.sintomas[sintoma.id], sintoma.valor) || 0;
+            // console.log(`evaluacionGeneral: ${ enfermedad.nombre } -> ${ Math.min(enfermedad.sintomas[sintoma.id], sintoma.valor) }\t${ sintoma.nombre } -> ${ sintoma.valor }`);
+        });
+        console.log(`evaluacionGeneral: ${ enfermedad.nombre }: ${ sumaMembresias }`);
+
+        if(sumaMembresias > umbral) {
+            if(resultados[0] && resultados[0].membresia < sumaMembresias) {
+                resultados = [{
+                        membresia: sumaMembresias,
+                        enfermedad
+                    }];
+            } else if ((resultados[0] && resultados[0].membresia == sumaMembresias) || !resultados[0]) {
+                resultados.push({
+                    membresia: sumaMembresias,
+                    enfermedad
+                });
+            }
+        }
+        
+        resultados.forEach(el => {
+            console.log(`evaluacionGeneral: resultado: ${ el.enfermedad.id } \t ${ el.membresia }`);
         })
     });
 
+    console.log(`evaluacionGeneral: sumaMembresias: ${ sumaMembresias }`);
+
+    let hSintomas = ``;
+    resultados.forEach(res => {
+        hSintomas = `
+        <li>
+            <h6>${ res.enfermedad.nombre }</h6>
+            <p class="card-text">${ res.enfermedad.descripcion }</p>
+            <p class="card-text">Recomendación: ${ res.enfermedad.recomendacion }</p>
+        </li>
+        `;
+    });
+
+    if (resultados == 1) {
+        modalResultados.html(`
+            <!-- <h5 style="padding-top: 2%;">Sintomas</h6> -->
+            <p>Esta es la enfermedad que mejor cuadra con sus síntomas:</p>
+            <ul>
+                ${ hSintomas }
+            </ul>
+        `);
+    } else if (resultados > 1) {
+        modalResultados.html(`
+            <!-- <h5 style="padding-top: 2%;">Sintomas</h6> -->
+            <p>Estas son las enfermedades que mejor cuadran con sus síntomas:</p>
+            <ul>
+                ${ hSintomas }
+            </ul>
+        `);
+    } else {
+        modalResultados.html(`<p>Ninguna enfermedad concuerda con sus síntomas`);
+    }
     // sumaMembresias = 0;
 };
 
 const evaluacionEspecifica = () => {
+    let slidersSintomas = $('.slider-especifico');
+    let modalResultados = $('#modal-resultados');
     
 };
 
